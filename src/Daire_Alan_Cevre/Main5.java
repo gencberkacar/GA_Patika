fa76c1091a8d08d39be714b8923c8895b8301060
gencberkacar <154082410+gencberkacar@users.noreply.github.com>
2024-02-20 17:48:54 +0300

    Patika

--- src/Daire_Alan_Cevre/Main5.java
+++ src/Daire_Alan_Cevre/Main5.java
@@ -4,19 +4,20 @@ public class Main5 {
     public static void main(String[] args) {
         //  veri nesnesi oluşturuldu
         Scanner veri = new Scanner(System.in);
+        double yaricap,aci,PI,alan;
 
         // Kullanıcıdan dairenin yarı çapını ve merkez açısının ölçüsü alındı
         System.out.print("Dairenin yarı çapını girin: ");
-        double yaricap = veri.nextDouble();
+         yaricap = veri.nextDouble();
 
         System.out.print("Merkez açısının ölçüsünü girin: ");
-        double aci = veri.nextDouble();
+         aci = veri.nextDouble();
 
         // Pi sayısını sabit olarak tanımlandı
-        final double PI = 3.14;
+         PI = 3.14;
 
         // Alanı hesapla
-        double alan = (PI * yaricap * yaricap * aci) / 360;
+         alan = (PI * yaricap * yaricap * aci) / 360;
 
         // Sonucu yazdır
         System.out.println("Daire Diliminin Alanı: " + alan);
