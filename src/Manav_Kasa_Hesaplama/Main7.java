8f62eb4ba5a066273e6eef41fc3f6af2cc9dfd5a
gencberkacar <154082410+gencberkacar@users.noreply.github.com>
2024-02-20 18:02:41 +0300

    Patika

--- src/Manav_Kasa_Hesaplama/Main7.java
+++ src/Manav_Kasa_Hesaplama/Main7.java
@@ -12,24 +12,24 @@ public class Main7 {
         double patlicanFiyat = 5.00;
         double armutKilo,domatesKilo,elmaKilo,muzKilo,toplamTutar,patlicanKilo;
 
-        // Scanner nesnesi oluşturuldu
-        Scanner scanner = new Scanner(System.in);
+        // veri nesnesi oluşturuldu
+        Scanner veri = new Scanner(System.in);
 
         // Kullanıcıdan meyve miktarlarını al
         System.out.print("Armut Kaç Kilo? : ");
-         armutKilo = scanner.nextDouble();
+         armutKilo = veri.nextDouble();
 
         System.out.print("Elma Kaç Kilo? : ");
-         elmaKilo = scanner.nextDouble();
+         elmaKilo = veri.nextDouble();
 
         System.out.print("Domates Kaç Kilo? : ");
-         domatesKilo = scanner.nextDouble();
+         domatesKilo = veri.nextDouble();
 
         System.out.print("Muz Kaç Kilo? : ");
-         muzKilo = scanner.nextDouble();
+         muzKilo = veri.nextDouble();
 
         System.out.print("Patlıcan Kaç Kilo? : ");
-         patlicanKilo = scanner.nextDouble();
+         patlicanKilo = veri.nextDouble();
 
         // Toplam tutarı hesapla
          toplamTutar = armutFiyat * armutKilo + elmaFiyat * elmaKilo + domatesFiyat * domatesKilo + muzFiyat * muzKilo + patlicanFiyat * patlicanKilo;
