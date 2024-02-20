b2950e33c09d0b1f70ba51399415693b665cdaca
gencberkacar <154082410+gencberkacar@users.noreply.github.com>
2024-02-20 14:15:43 +0300

    Ders Ortalamasi

--- src/Proje1/Main.java
+++ src/Proje1/Main.java
@@ -1,17 +1,46 @@
-public class Main
-
-{
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
+        //Degiskenleri Atadık
+         int mat,fizik,kimya,turkce,tarih,muzik;
 
-        int a = 6, b = 1, c = 9;
-        boolean kosul1 = a > b;
-        boolean kosul2 = a >= c;
-        boolean sonuc = kosul1 || kosul2;
-        boolean sonuc2 = kosul1 && kosul2;
-        boolean sonuc3 = !((kosul1 || kosul2));
-        String str = sonuc ? "Doğru" : "Yanlış";
+         //Scanner sınıfımızı tanımladık
+        Scanner deger = new Scanner(System.in);
 
-        System.out.println(str);
+        //Kullanıcıdan Mat degerini aldık
+        System.out.print("Matematik Notunuz: ");
+        mat = deger.nextInt();
+
+        //Kullanıcıdan Fizik degerini aldık
+        System.out.print("Fizik Notunuz: ");
+        fizik = deger.nextInt();
+
+        //Kullanıcıdan Kimya degerini aldık
+        System.out.print("Kimya Notunuz: ");
+        kimya = deger.nextInt();
+
+        //Kullanıcıdan Turkce degerini aldık
+        System.out.print("Türkçe Notunuz: ");
+        turkce = deger.nextInt();
+
+        //Kullanıcıdan Tarih degerini aldık
+        System.out.print("Tarih Notunuz: ");
+        tarih = deger.nextInt();
+
+        //Kullanıcıdan Müzik degerini aldık
+        System.out.print("Müzik Notunuz: ");
+        muzik = deger.nextInt();
+
+        //Derslerin Toplamani toplam değişkenine atadık
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+
+        //toplam Değişkenini Toplam ders sayısına bölüm ortalama değişkenine atadık
+        double ortalama = toplam / 6.0;
+
+        //Ortalamayı yazdırdık
+        System.out.println("Ortalamanız : " + ortalama);
+
+
     }
 
 }
