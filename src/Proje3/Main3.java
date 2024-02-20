d2122fd6e9f59c9a72a75bd4dfb4e88f633f91f1
gencberkacar <154082410+gencberkacar@users.noreply.github.com>
2024-02-20 17:27:40 +0300

    Patika

--- src/Proje3/Main3.java
+++ src/Proje3/Main3.java
@@ -1,7 +1,31 @@
 package Proje3;
+import java.util.Scanner;
 
-public class Main3
 
-{
+public class Main3 {
+
+    public static void main(String[] args) {
+        double a,b,c,u,alan;
+        // Scanner nesnesi oluşturuldu
+        Scanner girdi = new Scanner(System.in);
+
+        // Kullanıcıdan üçgenin kenar uzunluklarını al
+        System.out.print("1. kenar uzunluğunu girin: ");
+         a = girdi.nextDouble();
+
+        System.out.print("2. kenar uzunluğunu girin: ");
+         b = girdi.nextDouble();
+
+        System.out.print("3. kenar uzunluğunu girin: ");
+         c = girdi.nextDouble();
+
+        // Yarı çevreyi hesapla (u)
+         u = (a + b + c) / 2;
+
+        // Alanı hesapla
+         alan = Math.sqrt(u * (u - a) * (u - b) * (u - c));
+
+        // Sonucu yazdır
+        System.out.println("Üçgenin Alanı: " + alan);
+    }
 }
-
