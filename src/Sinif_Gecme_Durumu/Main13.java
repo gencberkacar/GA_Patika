4bd49e7b4f5d615089bafdd875caf5ff79578cf5
gencberkacar <154082410+gencberkacar@users.noreply.github.com>
2024-02-21 23:30:17 +0300

    Patika

---
+++ src/Sinif_Gecme_Durumu/Main13.java
@@ -0,0 +1,24 @@
+package Sinif_Gecme_Durumu;
+import java.util.Scanner;
+public class Main13 {
+    public static void main(String[] args) {
+    Scanner veri = new Scanner(System.in);
+    int mat, fizik, turkce, kimya, muzik, ort ;
+
+    System.out.print("Matematik Notunuzu Giriniz : ");
+        mat = veri.nextInt();
+        System.out.print("Fizik Notunuzu Giriniz : ");
+        fizik = veri.nextInt();
+        System.out.print("Türkçe Notunuzu Giriniz : ");
+        turkce = veri.nextInt();
+        System.out.print("Kimya Notunuzu Giriniz : ");
+        kimya = veri.nextInt();
+        System.out.print("Müzik Notunuzu Giriniz : ");
+        muzik = veri.nextInt();
+
+        if(mat <=0 && mat>100){
+            ort = mat;
+                    if()
+        }
+    }
+}
